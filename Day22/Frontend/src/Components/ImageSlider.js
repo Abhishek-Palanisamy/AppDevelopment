d8f08f2533f59376b8e116b5b8e9b1af88ed7dda
Abhishek P <110532245+Abhishek-Palanisamy@users.noreply.github.com>
2023-10-16 09:45:54 +0530

    Add files via upload

---
+++ Day22/Frontend/src/Components/ImageSlider.js
@@ -0,0 +1,12 @@
+
+export const ImageSlider=[{
+    image:"https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg"
+},
+{
+    image:"https://homezonline.in/wp-content/uploads/2022/09/Double-storey-home-with-alluring-exterior.jpg"
+},
+{
+    image:"https://media.istockphoto.com/id/1026205392/photo/beautiful-luxury-home-exterior-at-twilight.jpg?s=612x612&w=0&k=20&c=HOCqYY0noIVxnp5uQf1MJJEVpsH_d4WtVQ6-OwVoeDo="
+}
+
+] ;
